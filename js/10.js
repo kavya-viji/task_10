a4f419dd7fa4fd90f0fe340998673054a2bb53f7
Admin <kavyak6999@gmail.com>
2022-12-15 11:04:28 +0530

    even/odd

---
+++ js/10.js
@@ -0,0 +1,87 @@
+/*for (a=0;a<=20;a++)
+{
+console.log("numbers"+" "+a);
+}*/
+
+/*for (let a = 0; a <= 20;  a++)
+{
+  if(a%2==0)
+  {
+    console.log("numbers"+" "+ a);
+  }
+} */ 
+
+/*for ( a = 22; a >= 0;  a--)
+{
+  if(a%2==0)
+  {
+    console.log("numbers"+" "+ a);
+  }
+} */ 
+  
+  
+/*let a = 0;
+while (a <= 20)
+{
+	{
+		console.log("number" + a);
+	}
+	a++;
+}	*/
+
+  
+/*let a = 20;
+while (a >= 0)
+{
+	if(a%2==0)
+	{
+		console.log("number" + a);
+	}
+	a--;
+}	*/
+
+/*let a =0
+do
+{
+	console.log("numbers"+" "+a);
+    a++;	
+}
+while(a <= 20);*/
+
+var num=parseInt(prompt("enter number :"));
+n=0
+for(a=num;num%2==0&&a==num;a++)
+{
+	document.write(num+" is even");
+	n+=1;
+}
+for(n1=n;n1==0;n1++)
+{
+	document.write(num+" is odd");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
